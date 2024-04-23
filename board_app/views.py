b89845d7be3c3e2b79c4580797f08e04e8ae0ba2
a1163123 <a1163123@gmail.com>
2024-04-23 22:44:13 +0900

    red_taeg board

---
+++ board_app/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Board
+from .serializers import BoardSerializer
+
+class BoardListCreate(generics.ListCreateAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+
+class BoardRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
